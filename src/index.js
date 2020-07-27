5b0e10f8f50e62ad22dd9806a9b106ecd2224b72
nzambello <nzambello@protonmail.com>
2020-07-27 10:39:46 +0200

    fix package imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { default as MultilingualWidget } from 'volto-multilingual-widget/MultilingualWidget'
+export { default as MultilingualWidget } from './MultilingualWidget'
 
 export default config => config

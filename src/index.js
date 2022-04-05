ed6119065ef4c267a4e0e8d12412c410b9c2d46f
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-04-05 13:02:11 +0200

    chore: removed unuesed dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import loadable from '@loadable/component';
 export { default as MultilingualWidget } from './MultilingualWidget';
 
-const applyConfig = (config) => {
-  config.settings.loadables = {
-    ...config.settings.loadables,
-    draftJsExportHtml: loadable.lib(() => import('draft-js-export-html')),
-  };
-};
-
-export default applyConfig;
+export default (config) => config;
